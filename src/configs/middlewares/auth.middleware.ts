25d3edad9d29a67f6cdb9d561cd591f3de5f4305
Codeliner <codeliner96@gmail.com>
2022-04-24 17:57:57 +0900

    Auth Middleware modify (Added comment).

--- src/configs/middlewares/auth.middleware.ts
+++ src/configs/middlewares/auth.middleware.ts
@@ -9,6 +9,8 @@ import { Request, Response } from 'express';
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, _: Response, next: (error?: any) => void) {
     const isLocalEnvironment = process.env.NODE_ENV === 'local';
+
+    // Session has logged in user data? && Is production or development environment?
     if (!req.session.loggedInUser && !isLocalEnvironment) {
       throw new UnauthorizedException('로그인이 필요합니다.');
     }
